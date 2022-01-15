b061df0d35b880fe93572988d205e245158bb4ee
YunL97 <leeys9932@naver.com>
2022-01-15 22:54:32 +0900

    coredata test추가

--- OneDayTodo/Controller/AddTodoViewController.swift
+++ OneDayTodo/Controller/AddTodoViewController.swift
@@ -6,24 +6,58 @@
 //
 
 import UIKit
+import CoreData
 
 class AddTodoViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    var testModel:testModel?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //텍스트필드 테두리 변경
+        textField.layer.borderColor = UIColor.black.cgColor
+        textField.layer.borderWidth = 1.0
+        
+        
+        
+        
+//        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "alarm"), style: .plain, target: self, action: #selector(popToPrevious))
+        
+//        self.navigationItem.backButtonTitle = "뒤로"
+//        self.present(addVC, animated: true, completion: nil)
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    @IBAction func testAction(_ sender: Any) {
+        //NSManagedObjectContext
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+    guard    let entityDesciption = NSEntityDescription.entity(forEntityName: "Test", in: context) else {
+        return
     }
-    */
-
+    
+    //그 모델 그대로 가져옴
+    guard let object = NSManagedObject(entity: entityDesciption, insertInto: context) as? Test else{
+        return
+    }
+        object.test = "model 에 들어가는지 테스트1"
+//        object.test = "model 에 들어가는지 테스트2"
+        
+        let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
+        
+        appDelegate.saveContext()
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+//    @objc func popToPrevious() {
+//        self.navigationController?.popViewController(animated: true)
+//    }
+    
 }
--- OneDayTodo/Controller/ViewController.swift
+++ OneDayTodo/Controller/ViewController.swift
@@ -12,17 +12,104 @@ import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var todoTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        var titleName = UILabel()
-       
-        self.title = "습관에는 의지가 필요하지 않아요. 습관이 쌓이면 인생이 바뀝니다"
         
-        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor:UIColor.blue]
+        self.title = "억지로하는게 아닌 당연히 해야하는것"
         
-  
+//        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor:UIColor.blue]
+        
+//        todoTableView.delegate = self
+//        todoTableView.dataSource = self
+//
+        self.makeLeftEditButton()
+        self.makeRightAddButton()
+        self.navistatbarColor()
+        
+        
+        
+        var test = [Test]()
+        let appdelegate = UIApplication.shared.delegate as! AppDelegate
+        let fetchRequest: NSFetchRequest<Test> = Test.fetchRequest()
+        
+        let context = appdelegate.persistentContainer.viewContext
+         
+         do {
+             //
+             test = try context.fetch(fetchRequest)
+             
+             print(test.count)
+             print("aa")
+         }catch {
+             printContent(error)
+         }
+        
+        
+        var testmodel1 = testModel()
+        testmodel1.test = test
+        print(testmodel1)
+        
     }
-
-
+    
+    func makeLeftEditButton() {
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+    }
+    
+    //네비게이션 + 버튼 추가
+    func makeRightAddButton() {
+        let item = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTodo))
+        
+//        item.tintColor = .blue
+        self.navigationItem.rightBarButtonItem = item
+    }
+    
+    //네비게이션바, 스테이터스바 색 변경
+    func navistatbarColor() {
+        let barAppearence = UINavigationBarAppearance()
+        barAppearence.backgroundColor = UIColor(red: 20/255, green: 30/255, blue: 40/255, alpha: 0.2)
+    
+        self.navigationController?.navigationBar.scrollEdgeAppearance = barAppearence
+        self.navigationController?.navigationBar.standardAppearance = barAppearence
+    }
+    
+    @objc func addTodo() {
+        print("add");
+        let addVC = AddTodoViewController.init(nibName: "AddTodoViewController", bundle: nil)
+        
+        
+        self.present(addVC, animated: true, completion: nil)
+        
+        //네비게이션바 push
+//        self.navigationController?.pushViewController(addVC, animated: true)
+//        self.navigationController?.navigationBar.backIndicatorImage = UIImage(systemName: "alarm")
+//
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+    
+        
+        
+        
+    }
+    
+    
+    
 }
+
+//extension ViewController:UITableViewDelegate, UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+////        return
+//    }
+//
+//
+//}
 
