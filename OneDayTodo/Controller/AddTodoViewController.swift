a06e9c5c6e82ea3fc969d051f8aae9e23c608dcb
YunL97 <leeys9932@naver.com>
2022-01-17 23:20:13 +0900

    plus, delete button 추가

--- OneDayTodo/Controller/AddTodoViewController.swift
+++ OneDayTodo/Controller/AddTodoViewController.swift
@@ -5,14 +5,18 @@
 //  Created by 이윤식 on 2022/01/14.
 //
 
+
+
 import UIKit
 import CoreData
 
 class AddTodoViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
-    var testModel:testModel?
     
+
+    //NSManagedObjectContext
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,44 +24,16 @@ class AddTodoViewController: UIViewController {
         textField.layer.borderColor = UIColor.black.cgColor
         textField.layer.borderWidth = 1.0
         
-        
-        
-        
-//        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "alarm"), style: .plain, target: self, action: #selector(popToPrevious))
-        
-//        self.navigationItem.backButtonTitle = "뒤로"
-//        self.present(addVC, animated: true, completion: nil)
     }
     
-//
-//    @IBAction func testAction(_ sender: Any) {
-//        //NSManagedObjectContext
-//        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-//
-//    guard    let entityDesciption = NSEntityDescription.entity(forEntityName: "Test", in: context) else {
-//        return
-//    }
-//
-//    //그 모델 그대로 가져옴
-//    guard let object = NSManagedObject(entity: entityDesciption, insertInto: context) as? Test else{
-//        return
-//    }
-//        object.test = "model 에 들어가는지 테스트1"
-////        object.test = "model 에 들어가는지 테스트2"
-//
-//        let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
-//
-//        appDelegate.saveContext()
-//
-//        dismiss(animated: true, completion: nil)
-//    }
     
-    
-    
-    
-    
-//    @objc func popToPrevious() {
-//        self.navigationController?.popViewController(animated: true)
-//    }
+    @IBAction func saveTodo(_ sender: Any) {
+        // todoList 테이블 가져오기
+        //NSentityDesciption
+    guard  let entityDesciption = NSEntityDescription.entity(forEntityName: "TodoList", in: context) else {
+        return
+    }
+        
+    }
     
 }
