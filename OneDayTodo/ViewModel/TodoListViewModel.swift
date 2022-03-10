645b690d39825fdf41c4c91901ea6f63d2070ab8
YunL97 <leeys9932@naver.com>
2022-03-10 22:45:47 +0900

    resetcount 함수 추가

--- OneDayTodo/ViewModel/TodoListViewModel.swift
+++ OneDayTodo/ViewModel/TodoListViewModel.swift
@@ -6,15 +6,39 @@
 //
 
 import Foundation
-
+import CoreData
+import UIKit
 
 struct TodoListViewModel{
-    let todoList:[TodoList]
+
 }
 
 extension TodoListViewModel {
-    func numberOfRowInSection(_ section: Int) -> Int {
-        return self.todoList.count
+
+    func resetCount(_ todoList: [TodoList]) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        if todoList.count != 0{
+            for i in 0...todoList.count - 1 {
+                //                    print(todoList)
+                let fetchRequest:NSFetchRequest<TodoList> = TodoList.fetchRequest()
+                guard let hasUUID = todoList[i].uuid else {
+                    return
+                }
+                fetchRequest.predicate = NSPredicate(format: "uuid = %@", hasUUID as CVarArg)
+                do{
+                    let loadeddData = try context.fetch(fetchRequest)
+                    //print(loadeddData)
+                    loadeddData.first?.check = false
+                    let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
+                    
+                    //코어데이터에 저장
+                    appDelegate.saveContext()
+                }catch{
+                    print(error)
+                }
+            }
+           
+        }
     }
 }
 
