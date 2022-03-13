875ea14a771fc85cd1a3fbae3e314433d4f98e89
YunL97 <leeys9932@naver.com>
2022-03-14 00:11:58 +0900

    ViewModel 코드 edit 코드 추가

--- OneDayTodo/ViewModel/TodoListViewModel.swift
+++ OneDayTodo/ViewModel/TodoListViewModel.swift
@@ -10,7 +10,7 @@ import CoreData
 import UIKit
 
 struct TodoListViewModel{
-
+    let context:NSManagedObjectContext
 }
 
 extension TodoListViewModel {
@@ -38,6 +38,30 @@ extension TodoListViewModel {
                 }
             }
            
+        }
+    }
+    
+    func editMove(_ todoList:[TodoList]){
+        for i in 0...todoList.count - 1 {
+            //                    print(todoList)
+            let fetchRequest:NSFetchRequest<TodoList> = TodoList.fetchRequest()
+            guard let hasUUID = todoList[i].uuid else {
+                return
+            }
+            fetchRequest.predicate = NSPredicate(format: "uuid = %@", hasUUID as CVarArg)
+            do{
+                let loadeddData = try context.fetch(fetchRequest)
+                //                                            print(loadeddData)
+                loadeddData.first?.order = Int32(i)
+                let appDelegate = (UIApplication.shared.delegate as! AppDelegate )
+                appDelegate.saveContext()
+                //이 fetchData때문에 오류가 나는거였음
+                //fetchData에서 todoList에 데이터를 담아서 꼬이는거였네 for문돌때마다 가져와서 데이터가 꼬이는거였음
+                //fetchData()
+                //todoTableView.reloadData()
+            }catch{
+                print(error)
+            }
         }
     }
 }
